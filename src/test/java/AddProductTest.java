2ea0aa60dbba44a9a268b4bc1b022152984dfde6
ArtyomLV <artlv@X.local>
2020-02-11 20:38:55 +0300

    a litle refactor for test

--- src/test/java/AddProductTest.java
+++ src/test/java/AddProductTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AddProductTest {
@@ -17,12 +18,36 @@ public class AddProductTest {
   @Test
   public void FirstMethodAdd() throws Exception
   {
-    $(By.cssSelector(".basket > .wrapper")).click();
-    $(By.cssSelector(".basket-empty__button")).click();
+    setBasket();
+    addProduct();
+    set1CInMenu();
+    selectProductInList();
+    goToBasketAfter();
+    checkProductInBasket();
 //    $(By.cssSelector(".categories > Обмен с 1С")).click();
+  }
+
+  public void setBasket() {
+    $(By.cssSelector(".basket > .wrapper")).click();
+  }
+
+  public void addProduct() {
+    $(By.cssSelector(".basket-empty__button")).click();
+  }
+
+  public void set1CInMenu() {
     $(By.xpath("//a[contains(@class, 'categories__item')][23]")).click();
+  }
+
+  public void selectProductInList() {
     $(By.xpath("//app-card[.//h3[@class = 'app-title' and contains(.,'Интеграция 1С 8')]]")).click();
+  }
+
+  public void goToBasketAfter() {
     $(By.cssSelector(".plan-item-controls__basket-button")).click();
+  }
+
+  public void checkProductInBasket() {
     $(By.xpath("//*[contains(@class,'basket__list-item')]")).shouldBe(Condition.visible);
   }
 
