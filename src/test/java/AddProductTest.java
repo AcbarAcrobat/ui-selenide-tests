2e14d8022e3fb4fb9a7ffa65c3c53b4b4d9a419a
ArtyomLV <artlv@X.local>
2020-02-12 08:58:59 +0300

    refactoring

--- src/test/java/AddProductTest.java
+++ src/test/java/AddProductTest.java
@@ -4,7 +4,6 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AddProductTest {
@@ -27,27 +26,33 @@ public class AddProductTest {
 //    $(By.cssSelector(".categories > Обмен с 1С")).click();
   }
 
-  public void setBasket() {
+  public void setBasket()
+  {
     $(By.cssSelector(".basket > .wrapper")).click();
   }
 
-  public void addProduct() {
+  public void addProduct()
+  {
     $(By.cssSelector(".basket-empty__button")).click();
   }
 
-  public void set1CInMenu() {
+  public void set1CInMenu()
+  {
     $(By.xpath("//a[contains(@class, 'categories__item')][23]")).click();
   }
 
-  public void selectProductInList() {
+  public void selectProductInList()
+  {
     $(By.xpath("//app-card[.//h3[@class = 'app-title' and contains(.,'Интеграция 1С 8')]]")).click();
   }
 
-  public void goToBasketAfter() {
+  public void goToBasketAfter()
+  {
     $(By.cssSelector(".plan-item-controls__basket-button")).click();
   }
 
-  public void checkProductInBasket() {
+  public void checkProductInBasket()
+  {
     $(By.xpath("//*[contains(@class,'basket__list-item')]")).shouldBe(Condition.visible);
   }
 
